75377ca14a39f638da7b77ea9e3b5fbe4f55b123
AlaaAbu1 <>
2021-06-03 13:14:01 +0000

    JS Updates

---
+++ script.js
@@ -0,0 +1,36 @@
+// This is a comment
+/* This is a comment test 2
+*/
+
+var welcomeLetter;
+  welcomeLetter = "Welcome to Creative Lab";
+
+console.log(welcomeLetter);
+
+
+
+
+var id;
+  id = "Fill Yor Name";
+console.log(id);
+
+
+
+
+var destance = 100;
+if (destance <= 25) {
+  alert("mmmmm")
+} else {
+  alert("Welcome")
+}
+if (destance <= 100) {
+  alert("You are here")
+} else if(destance > 100) {
+  alert("You are far")
+}else{
+  alert("come back")
+}
+
+
+var destance = prompt("Where is your place?");
+console.log(destance);
