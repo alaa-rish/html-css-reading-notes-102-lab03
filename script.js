e4abdf36a44db6802276de4c5ef99b534d314245
AlaaAbu1 <>
2021-06-06 23:52:41 +0000

    JS

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ if (destance <= 100) {
 }
 if (destance <= 100) {
   alert("You are here")
-} else if(destance > 100) {
+} else if(destance > 100){
   alert("You are far")
 }else{
   alert("come back")
@@ -34,9 +34,58 @@ if (destance <= 100) {
 
 var name = prompt("Whats Your Name?");
 console.log(name);
-alert("Welcome " + name)
+alert("Welcome " + name);
 
 
 
 
-document.write("<p style='color:black;'> Welcome " + name + " To our Creative HUB </p>")
+document.write("<p style='color:black;'> Welcome " + name + " To our Creative HUB </p>");
+
+
+
+var design = prompt ("Are you senior, junior, creative?");
+while (design !== "senior" && design !== "junior" && design !== "creative") {
+ design = prompt  ("Are you senior, junior, creative?")
+}
+
+
+if (design == "senior")
+{
+  document.write ("<img src='https://miro.medium.com/max/4186/1*33khN9zpFCMWnF8pLqeVTQ.png'width='200px' hight='200px'/>");
+}
+
+else if (design == "junior"){document.write ("<img src='https://miro.medium.com/max/3210/1*zm_F9oKglOhapcdak3DEUw.jpeg'width='200px' hight='200px'/>");
+}
+else if (design == "creative")
+{document.write ("<img src='https://assets.website-files.com/5d9f2837a121e08ad998b708/5d9f29e2dd291bc632a79b09_Design-Thinking-Paradigm.jpeg' width='200px' hight='200px'/>");
+}
+
+
+var count = prompt("how many stars do you want to see?");
+var i = 0;
+while (i < count) {
+  document.write ("<img src='https://toppng.com/uploads/preview/old-star-transparent-png-clip-art-jpg-free-transparent-background-gold-star-11562882295js8ukwj32d.png' width='200px' hight='200px'/>")
+  i++;
+}
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
